c0737669b787f53603e12dc6774fb43775aba7c4
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-09-11 11:51:20 +0900

    9/11

---
+++ 불변객체확인.py
@@ -0,0 +1,13 @@
+# 파이썬
+x = 10  # immutable - 변경 불가능 / (일반 변수의 모임이 튜플)
+y = x  # y는 값의 값을 대입 받았으므로 10
+x = 20  # 기존의 x 변수의 값이 변경된 것이 아니라 새롭게 생성 됨
+print(x) # 20
+print(y) # 10
+
+# 자바
+# int x = 10;
+# unt y = x; # y도 현재 값은 10
+# x = 20;
+# System.out.println("x : " + x); # 20
+# System.out.println("y : " + y); # 10
