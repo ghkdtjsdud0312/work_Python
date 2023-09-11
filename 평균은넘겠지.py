b006dc41eb101ce82c3671f61e975e68d81fa652
ghkdtjsdud0312 <ghkdtjsdud0312@gmail.com>
2023-09-11 17:45:02 +0900

    9/11

--- 평균은넘겠지.py
+++ 평균은넘겠지.py
@@ -3,6 +3,7 @@ def over_rate() :
     ls = list(map(int,input().split()))
     average = sum(ls[1:])/len(ls[1:])
     cnt = 0
-    for i in range(1,len(ls)):
+    for i in range(1,len(ls)) :
+        if ls[i] > average: cnt+=1
 
 
